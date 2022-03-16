fdf032fe4ba42b2a09d1dd09faa179fb9a26901a
Matheus Webber <matheus.webber@hotmail.com>
2022-03-16 20:26:27 -0300

    codigo completamente comentado e pequeno erro na implementação foi corrigido (nao era retornada a melhor coloracao do grafo)

--- src/grafos/Grafo.java
+++ src/grafos/Grafo.java
@@ -5,10 +5,16 @@ import java.util.ArrayList;
 public class Grafo<type>{
 	private ArrayList<ArrayList<Vertice<type>>> grafo;
 	
+	//Construtor básico para criar um grafo vazio
 	public Grafo() {
 		grafo = new ArrayList<ArrayList<Vertice<type>>>();
 	}
 	
+	//Construtor para copiar um grafo que foi passado como parâmetro para o grafo que chamou a função
+	//Faz isso inicializando o grafo chamador da função como vazio e aos poucos passando as informaçoes do grafo original
+	//Primeiro cria os vértices e coloca eles em um ArrayList de vértices para depois colocar esse ArrayList no grafo 
+	//com o objetivo de inicializar esse novo grafo com os vértices do grafo original
+	//Após isso adiciona as arestas em em cada ArrayList de vértices 
 	public Grafo(Grafo<type> grafo) {
 		ArrayList<ArrayList<Vertice<type>>> elementosGrafo = grafo.getElementosGrafo();
 		this.grafo = new ArrayList<ArrayList<Vertice<type>>>();
@@ -28,14 +34,18 @@ public class Grafo<type>{
 		}
 	}
 	
+	//Função que retorna um ArrayList de ArrayList de vertices que seria o grafo
 	public ArrayList<ArrayList<Vertice<type>>> getElementosGrafo() {
 		return grafo;
 	}
 	
+	//Função que retorna o tamanho do grafo
 	public Integer size() {
 		return grafo.size();
 	}
 	
+	//Função que retorna um ArrayList de Vertices que representa os vértices do grafo 
+	//e faz isso adicioando o primeiro elemento de cada lista do grafo em um novo ArrayList de vertices e no final retorna esse ArrayList
 	public ArrayList<Vertice<type>> getVertices() {
 		ArrayList<Vertice<type>> vertices = new ArrayList<Vertice<type>>();
 		for (Integer i = 0; i < grafo.size(); i++) {
@@ -44,6 +54,8 @@ public class Grafo<type>{
 		return vertices;
 	}
 	
+	//Função que retorna a posição de um determinado vértice caso ele exista no grafo e caso não existe retorna -1
+	//e faz isso percorrendo o grafo e verificando se o dado do primeiro elemento de cada ArrayList do grafo corresponde ao dado procurado 
 	public Integer getVertice(type dado) {
 		for (Integer i = 0; i < grafo.size(); i++) {
 			if (grafo.get(i).get(0).getDado().equals(dado)) {
@@ -53,6 +65,8 @@ public class Grafo<type>{
 		return -1;
 	}
 	
+	//Função que tenta retornar um objeto vértice que corresponde ao mesmo vértice que tem o dado que foi passado como parâmetro 
+	//e faz isso percorrendo o grafo e verificando se o dado do primeiro elemento de cada ArrayList do grafo corresponde ao dado procurado 
 	public Vertice<type> getVerticeV(type dado) {
 		for (Integer i = 0; i < grafo.size(); i++) {
 			if (grafo.get(i).get(0).getDado().equals(dado)) {
@@ -62,6 +76,10 @@ public class Grafo<type>{
 		return null;
 	}
 	
+	//Função que adiciona um vértice ao grafo com o dado que foi passado como parâmetro
+	//e faz isso veritifando se já existe um vértice com esse dado, criando um novo objeto vértice ou pegando um que exite no grafo
+	//e cria um novo ArrayList de vértices.
+	//Após isso adiciona o novo vértice ao novo ArrayList e adiciona esse ArryaList no grafo
 	public void addVertices(type dadoVert) {
 		Vertice<type> vertice;
 		Integer indexVert = getVertice(dadoVert);
@@ -75,6 +93,9 @@ public class Grafo<type>{
 		grafo.add(listaVertice);
 	}
 	
+	//Função que retorna as arestas do grafo e faz isso criando um novo ArrayList de vértices que representa as arestas que vão ser retornadas
+	//depois disso percorre o grafo criando um vértice inicial e depois percorrendo o ArrayLisr que contem esse vértice
+	//para que seja adicionado esse vértice e seus vértices vizinhos no ArrayList de vértices para no final retornar eles
 	public ArrayList<Vertice<type>> getArestas(){
 		ArrayList<Vertice<type>> arestas = new ArrayList<Vertice<type>>();
 		Vertice<type> verticeInicial;
@@ -88,6 +109,11 @@ public class Grafo<type>{
 		return arestas;
 	}
 	
+	//Função que procura se existe uma aresta entre dois vértices.
+	//Recebe um grafo, um vértice em que vai ser procurada a aresta  e o vértice que queremos saber se tem uma aresta com o VertInicio
+	//e retorna true caso exista uma aresta entre o vértice e false caso contrário
+	//e faz isso procurando no grafo um vértice que seja igual ao vértice que esta sendo procurado para achar a aresta
+	//depois disso percorrer o ArrayList que contem esse vértice para procurar o vértice que queremos saber se tem uma aresta
 	public Boolean buscaAresta(Grafo<type> grafo, Vertice<type> VertInicio, Vertice<type> dadoVertFim) {
 		for (Integer i = 0; i < grafo.size(); i++) {
 			if (grafo.getElementosGrafo().get(i).get(0).getDado().equals(dadoVertFim)) {
@@ -101,6 +127,11 @@ public class Grafo<type>{
 		return false;
 	}
 	
+	//Função para adicionar uma aresta para o grafo.
+	//Recebe dois dados que representam os vértices que vão se ligar através de uma aresta
+	//Essa função cria dois novos vértices com os dados que foram passados como parâmetro
+	//e depois disso percorre o grafo para saber se o grafo contem o primeiro vértice  e caso encontre adiciona o segundo vértice em seu ArrayList
+	//após isso faz o mesmo processo com o segundo vertice mas dessa vez adicionando o primeiro vértice em seu ArrayList caso seja encontrado
 	public void addAresta(type dadoVertInicio, type dadoVertFim) {
 		Vertice<type> verticeInicio = new Vertice<type>(dadoVertInicio);
 		Vertice<type> verticeFim = new Vertice<type>(dadoVertFim);
@@ -118,6 +149,10 @@ public class Grafo<type>{
 		}
 	}
 	
+	//Função para adicionar uma aresta para o grafo.
+	//Recebe dois vértices que vão ser ligados através de uma aresta
+	//Essa função percorre o grafo para saber se o grafo contem o primeiro vértice  e caso encontre adiciona o segundo vértice em seu ArrayList
+	//após isso faz o mesmo processo com o segundo vertice mas dessa vez adicionando o primeiro vértice em seu ArrayList caso seja encontrado
 	public void addAresta(Vertice<type> VertInicio, Vertice<type> VertFim) {
 		for (Integer i = 0; i < grafo.size(); i++) {
 			if (grafo.get(i).get(0).getDado().equals(VertInicio.getDado())) {
--- src/main/Principal.java
+++ src/main/Principal.java
@@ -12,24 +12,47 @@ public class Principal {
 		grafo.addVertices("C");
 		grafo.addVertices("D");
 		grafo.addVertices("E");
+		grafo.addVertices("F");
+		
 		grafo.addAresta("A", "B");
 		grafo.addAresta("A", "C");
 		grafo.addAresta("B", "C");
-		grafo.addAresta("E", "C");
-		grafo.addAresta("E", "D");
+		grafo.addAresta("A", "D");
+		grafo.addAresta("B", "D");
+		grafo.addAresta("C", "D");
 		
-//		for (Integer i = 0; i < grafo.getVertices().size(); i++) {
-//			System.out.println("Vertice: " + grafo.getVertices().get(i));
-//		}
-//		for (Integer i = 0; i < grafo.getArestas().size(); i = i + 2) {
-//			System.out.println("Aresta: " + grafo.getArestas().get(i) + "," + grafo.getArestas().get(i+1));
-//		}
+		grafo.addAresta("A", "E");
+		grafo.addAresta("B", "E");
+		grafo.addAresta("C", "E");
+		grafo.addAresta("D", "E");
+		
+		grafo.addAresta("A", "F");
+		grafo.addAresta("B", "F");
+		grafo.addAresta("C", "F");
+		grafo.addAresta("D", "F");
+		grafo.addAresta("A", "F");
+		
+		Grafo<Integer> grafoInt = new Grafo<Integer>();
+		grafoInt.addVertices(1);
+		grafoInt.addVertices(2);
+		grafoInt.addVertices(3);
+		
+		grafoInt.addAresta(1, 2);
+		grafoInt.addAresta(1, 3);
+		grafoInt.addAresta(2, 3);
 		
 		Colorir<String> colorindo = new Colorir<String>();
 		Grafo<String> grafoRes = colorindo.coloreGrafo(grafo);
 		for (Integer i = 0; i < grafoRes.getVertices().size(); i++) {
             System.out.print("Vertice: " + grafoRes.getVertices().get(i).getDado());
             System.out.println(" - Frequência: " + grafoRes.getVertices().get(i).getFrequencia());
+        }
+		
+		Colorir<Integer> colorindo2 = new Colorir<Integer>();
+		Grafo<Integer> grafoRes2 = colorindo2.coloreGrafo(grafoInt);
+		for (Integer i = 0; i < grafoRes2.getVertices().size(); i++) {
+            System.out.print("Vertice: " + grafoRes2.getVertices().get(i).getDado());
+            System.out.println(" - Frequência: " + grafoRes2.getVertices().get(i).getFrequencia());
         }
 	}
 
